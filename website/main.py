c1bc01ea966864f35faac45146eec491de7ddf4c
Jonny <jonathon_lokken@yahoo.com>
2022-09-11 10:42:23 -0500

    Update main.py

--- website/main.py
+++ website/main.py
@@ -68,4 +68,4 @@ async def dashboard_server(guild_id):
 		return redirect(f'https://discord.com/oauth2/authorize?&client_id={app.config["DISCORD_CLIENT_ID"]}&scope=bot&permissions=8&guild_id={guild_id}&response_type=code&redirect_uri={app.config["DISCORD_REDIRECT_URI"]}')
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(host="0.0.0.0", port=80)
